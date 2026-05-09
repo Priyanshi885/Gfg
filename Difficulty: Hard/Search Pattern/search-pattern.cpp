1adc9accdfcde2a3132f61a17fb9aa306119984c
Priyanshi Saxena <saxenapriyanshi94@gmail.com>
2026-05-09 12:24:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Search Pattern/search-pattern.cpp
@@ -0,0 +1,70 @@
+class Solution {
+  public:
+  void computeLPS(string &pat, vector<int> &LPS, int M){
+      int len=0;
+      LPS[0] = 0;
+      int i=1;
+      while(i<M)
+      {
+          if(pat[i] == pat[len])
+          {
+              len++;
+              LPS[i] = len;
+              i++;
+          }
+          else
+          {
+              if(len!=0)
+              {
+                  len = LPS[len-1];
+              }
+              else
+              {
+                  if(len!=0){
+                      len = LPS[len-1];
+                  }
+                  else{
+                      LPS[i]=0;
+                      i++;
+                  }
+              }
+          }
+      }
+  }
+    vector<int> search(string &pat, string &txt) {
+        // code here
+        int N= txt.length();
+        int M= pat.length();
+        
+        vector<int>result;
+        
+        //create an LPS array
+        vector<int>LPS(M,0);
+        computeLPS(pat, LPS, M);
+        
+        //Apply KMP
+        int i=0;
+        int j=0;
+        while(i<N)
+        {
+            if(txt[i]==pat[j]){
+                i++;
+                j++;
+            }
+            if(j==M){
+                //found
+                result.push_back(i-M);
+                j=LPS[j-1];
+            }
+            else if(txt[i] != pat[j]){
+                if(j!=0){
+                    j=LPS[j-1];
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return result;
+    }
+};
